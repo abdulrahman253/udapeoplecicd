0ce51d877fd8aa78385b07c08d5186b7843177a3
abdulrahman <abdulrahmanheiba253@gmail.com>
2022-08-10 22:57:41 +0200

    0

--- .circleci/ansible/roles/configure-server/myenv.sh
+++ .circleci/ansible/roles/configure-server/myenv.sh
@@ -1,11 +1,9 @@
-export NODE_ENV=production
-export VERSION=1
 export TYPEORM_CONNECTION=postgres
 export TYPEORM_MIGRATIONS_DIR=./src/migrations
 export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
 export TYPEORM_MIGRATIONS=./src/migrations/*.ts
-export TYPEORM_HOST=localhost
-export TYPEORM_PORT=5532
+export TYPEORM_HOST=udapeople-db.ceessbwnsown.us-east-1.rds.amazonaws.com
+export TYPEORM_PORT=5432
 export TYPEORM_USERNAME=postgres
 export TYPEORM_PASSWORD=password
-export TYPEORM_DATABASE=glee
+export TYPEORM_DATABASE=postgres
